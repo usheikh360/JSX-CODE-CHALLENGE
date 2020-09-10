87ab7fb083b0dcedfae1fe97431b67c994424591
Umair <32969159+usheikh360@users.noreply.github.com>
2020-09-10 21:15:33 +0100

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+var React = require("react");
+var ReactDom = require("react-dom");
+
+ReactDom.render(
+  <div>
+    <h1> Favorite fruits </h1>
+    <ul>
+      <li>Apple</li>
+      <li>Banana</li>
+      <li>Peach</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
